6484936f3f6c2592783c1660d8ec62e9df449ea3
Gittagi ismin <Sani emaillin>
2024-03-28 15:40:25 +0500

    Innitial  commit

---
+++ main.js
@@ -0,0 +1,61 @@
+
+// ----------------------------------------1 chi topshiriq------------------------------------------
+// function urtaSon(a, b, c) {
+//     if ((a <= b && a >= c)) {
+//         return a
+//     } 
+//     if(a >= b && a <= c) {
+//         return a
+//     }
+//      if ((b <= a && b >= c)) {
+//         return b;
+//     }
+//     if(b >= a && b <= c) {
+//         return b
+//     }else c;
+// }
+// console.log(urtaSon(5, 24, 90));
+// ----------------------------------------2 chi topshiriq------------------------------------------
+// function summ(arr) {
+//     let plus = 0;
+//     let minus = 0;
+//     for (let i of arr) {
+//         if (i > 0) {
+//             plus++;
+//         }
+//          if (i < 0) {
+//             minus++;
+//         }
+//     }
+//     alert("Shuncha musbat son bor: " + plus);
+//     alert("Shuncha manfiy son bor: " + minus);
+// }
+// let arrayla = [5, -3, -5,];
+// summ(arrayla);
+// ----------------------------------------3 chi topshiriq------------------------------------------
+// function  minusChiqargich(arr){
+//     let a = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr [i] < 0){
+//             a += arr[i];
+//         }
+//     }
+//     return a;
+// }
+// alert(minusChiqargich([3,-9,2,1]));
+// ----------------------------------------4 chi topshiriq------------------------------------------
+// function kattaSon(a, b, c) {
+//     if ((a >= b && a >= c)) {
+//         return a
+//     } 
+//     if(b >= a && b >= c) {
+//         return b
+//     }
+//      if ((c >= a && c >= b)) {
+//         return c;
+//     }
+// }
+// console.log(kattaSon(5, 240, 90));
+
+
+
